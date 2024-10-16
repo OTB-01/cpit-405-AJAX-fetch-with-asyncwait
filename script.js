91f4f6dcb0e3a45c0e56ab9df7a5ee5267f838c6
unknown <yousef-1122@hotmail.com>
2024-10-16 11:46:03 +0300

    init

---
+++ script.js
@@ -0,0 +1,50 @@
+let btnFetch = document.getElementById('fetchSearch');
+let searchText = document.querySelector('input[type="text"]');
+let searchResults = document.getElementById("searchResults");
+
+
+// handle search button click
+btnFetch.addEventListener("click", function () {
+    // clear previous search results
+    searchResults.innerHTML = "";
+
+    console.log(` btn event ${searchText.value}`);
+    fetchWheather(searchText.value)
+});
+
+// fetch with async and await Example
+async function fetchWheather(keyword) {
+    var url = "https://api.weatherapi.com/v1/forecast.json";
+
+    var apiKey = "210a3b9a0a1b4feaa0275717241610";
+    var params = "key=" + apiKey + "&q=" + encodeURIComponent(keyword) + "&days=1&aqi=no&alerts=no";
+    if (!keyword) {
+        return;
+    }
+    var requestOptions = {
+        method: 'GET'
+    };
+
+    const response = await fetch(url + "?" + params, requestOptions); // Wait until the request completes.
+    const data = await response.json(); // waits until the response completes
+    console.log(` json object ${data}`);
+    processResponse(data);
+}
+
+function processResponse(respObj) {
+    console.log(` proccesing response ${respObj.data}`);
+
+    let nameElem = document.createElement("p");
+    nameElem.innerHTML = respObj.location.name;
+    searchResults.appendChild(nameElem);
+    
+    let temp = document.createElement("p");
+    temp.innerHTML = respObj.current.temp_c + " C";
+    searchResults.appendChild(temp);
+
+    let condition = document.createElement("p");
+    condition.innerHTML = respObj.current.condition.text;
+    searchResults.appendChild(condition);
+
+
+}
